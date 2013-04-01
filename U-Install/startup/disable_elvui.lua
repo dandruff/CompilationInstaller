b29577d02d3be0e027c9d36fc1daf8e98a146d5b
Cimplex <reubend@formetco.com>
2013-04-01 15:22:19 -0500

    Adding install ElvUI chat

--- U-Install/startup/disable_elvui.lua
+++ U-Install/startup/disable_elvui.lua
@@ -2,3 +2,23 @@ local _, E, L, C = nil, unpack(select(2, ...))
 
 -- Disable ElvUI Installer
 if ElvUI then E.ElvUIInstallCache = ElvUI[1].Install; ElvUI[1].Install = E.noop end
+
+local install = E.Install
+local function PreInstall(self, force)
+  E.ElvUIInstallCache()
+
+  -- Click next button twice
+  InstallNextButton:Click()
+  InstallNextButton:Click()
+  
+  -- Force Chat to install
+  InstallOption1Button:Click()
+  
+  -- Close the Install window
+  InstallCloseButton:Click()
+  
+  -- Call installer
+  install(self, force)
+end
+
+E.Install = PreInstall
